801056bb4bd5fc143cd9e5fb537670d5a3a56076
blaise594 <danielbrogers594@gmail.com>
2017-04-12 09:13:23 -0400

    trying to make form data save to database stil troubleshooting

--- GiftExchange/GiftExchange/Controllers/PresentstController.cs
+++ GiftExchange/GiftExchange/Controllers/PresentstController.cs
@@ -16,9 +16,25 @@ namespace GiftExchange.Controllers
             return View(presents);
         }
 
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            var Contents = collection["Contents"];
+            var GiftHint = collection["GiftHint"];
+            var ColorWrappingPaper = collection["ColorWrappingPaper"];
+            var Height = collection["Height"];
+            var Width = collection["Width"];
+            var Depth = collection["Depth"];
+            var Weight = collection["Weight"];
+            var IsOpened = collection["IsOpened"];
+
+            return RedirectToAction("Index");
         }
     }
 }
